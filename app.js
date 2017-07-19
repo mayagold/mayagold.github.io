31ed6bfb35cb7e6bf703522265dcb30d53d5d3ae
mayagold <maya.gold.1991@gmail.com>
2017-07-19 11:30:42 -0700

    test

--- app.js
+++ app.js
@@ -7,198 +7,166 @@
 // add animations to mushrooms so that they move around the page
 // add different animations for when you click on a poisonous and a magic mushroom
 // style the page and the modals
-// add the round# at the top of the page while playing, above Power
 // make the timer look cool / add animation
-
-
-
+// comment out code
+// fix restart button
 
 
 // JQUERY WINDOW ONLOAD
 
 $(() => {
 
-let score = 5;
-let time = 30;
+// Declare score and time variables outside of any object
+  let score = 5;
+  let time = 30;
 
-
-// First user prompt: Start button closes the intro modal and takes you to the game.
-// User will click on start button after reading the directions on the modal.
-// This will hide the modal (display:none) and show the mushroom-container div, which is the play space.
-
-
-const eventHandlers = {
-  startGame() {
-    $('.start-button').on('click', () => {
-      $('#modal-intro').hide();
-      $('.game-container').css('display', 'block');
-      $('.mushroom-container').css('display', 'block');
-      setUpRound();
-    });
-  },
-  clickShrooms() {
-    $('img').on('click', (e) => {
-      $(e.currentTarget).hide()});
-    $('img').on('click', (e) => {
-      if ($(e.currentTarget).attr('class') === 'poisonous') {
-        score--;
-        // alert("Be careful! That one was poisonous.")
-        $('#show-power').text('Power: ' + score);
-        console.log('lose point');
-          console.log(score);
-      } else if ($(e.currentTarget).attr('class') === 'magic') {
-        score++;
-        // alert("You found a magic mushroom!");
+// Event Handlers object
+  const eventHandlers = {
+    // start game: when you click the Start or Start Over button
+    startGame() {
+      score=5;
+      $('.start-button').on('click', () => {
+        $('#modal-intro').detach();
+        $('.modal').hide();
+        $('.game-container').css('display', 'block');
+        $('.mushroom-container').css('display', 'block');
+        setUpRound();
+        $('.start-button').off();
+      });
+    },
+    // clicking on mushrooms -- changes the score based on their class
+    clickShrooms() {
+      $('img').on('click', (e) => {
+        $(e.currentTarget).hide()});
+      $('img').on('click', (e) => {
+        if ($(e.currentTarget).attr('class') === 'poisonous') {
+          score--;
+          // alert("Be careful! That one was poisonous.")
           $('#show-power').text('Power: ' + score);
-        console.log('win point');
+          console.log('lose point');
+            console.log(score);
+        } else if ($(e.currentTarget).attr('class') === 'magic') {
+          score++;
+          // alert("You found a magic mushroom!");
+            $('#show-power').text('Power: ' + score);
+          console.log('win point');
+            console.log(score);
+        } else if ($(e.currentTarget).attr('class') === 'normal') {
+            $('#show-power').text('Power: ' + score);
+          console.log('no change');
           console.log(score);
-      } else if ($(e.currentTarget).attr('class') === 'normal') {
-          $('#show-power').text('Power: ' + score);
-        console.log('no change');
-        console.log(score);
-      }
-    });
-  },
-  go() {
-    $('.go-button').on('click', () => {
-      console.log('working');
-      $('.modal').hide();
-      $('.game-container').css('display', 'block');
-      $('.mushroom-container').css('display', 'block');
-      setUpRound();
-    });
-  },
-}
-
-
-
-
+        }
+      });
+    },
+    // when you click Keep Playing after winning a round
+    nextRound() {
+      $('#next-button').on('click', () => {
+        console.log('working');
+        $('.modal').hide();
+        $('.game-container').css('display', 'block');
+        $('.mushroom-container').css('display', 'block');
+        $('#next-button').off();
+        setUpRound();
+      });
+    },
+    // when you click Keep Playing after losing a round
+    repeatRound() {
+      $('#repeat-button').on('click', () => {
+        console.log('working');
+        $('.modal').hide();
+        $('.game-container').css('display', 'block');
+        $('.mushroom-container').css('display', 'block');
+        $('#repeat-button').off();
+        setUpRound();
+      });
+    },
+  }
 
 // the game round object
 
-const round = {
-  roundNumber: 1,
-  clearBoard () {
-    $('.mushroom-container').empty();
-  },
-  generateShrooms(num) {
-    for (i=0; i<num; i++) {
-      const shroom = $('<img src="images/Mushroom.png"/>');
-      shroom.css('max-height', '50px');
-      $('.mushroom-container').append(shroom);
-      let typeOfShroom = Math.random();
-      if (typeOfShroom < .5) {
-        shroom.addClass('normal');
-      } else if ((typeOfShroom >= .5) && (typeOfShroom <.8)) {
-        shroom.addClass('magic');
-      } else if (typeOfShroom >= .8) {
-        shroom.addClass('poisonous');
+  const round = {
+    // round number can be updated
+    roundNumber: 1,
+    // clear the board of any leftover mushrooms (at beginning of each round)
+    clearBoard () {
+      $('.mushroom-container').empty();
+    },
+    // generate a different number of shrooms depending on what round number it is, assigning them each a random class of poisonous, normal or magic
+    generateShrooms(num) {
+      for (i=0; i<num; i++) {
+        const shroom = $('<img src="images/Mushroom.png"/>');
+        shroom.css('max-height', '50px');
+        $('.mushroom-container').append(shroom);
+        let typeOfShroom = Math.random();
+        if (typeOfShroom < .5) {
+          shroom.addClass('normal');
+        } else if ((typeOfShroom >= .5) && (typeOfShroom <.8)) {
+          shroom.addClass('magic');
+        } else if (typeOfShroom >= .8) {
+          shroom.addClass('poisonous');
+        }
       }
+    },
+    // clear timer, reset timer to 30s
+    setTimer() {
+      time=30;
+      const timer = setInterval( ()=> {
+        time--
+        console.log(time);
+        $('h3').text("Timer: " + time + " sec");
+        if (time===0) {
+          clearInterval(timer);
+            checkForWin();
+        }
+      }, 1000);
+    },
+    // shows the repeat round modal
+    roundRepeat() {
+      $('.game-container').css('display', 'none');
+      $('#modal-repeat').css('display', 'block');
+      eventHandlers.repeatRound();
+    },
+    // shows the next round modal
+    roundNext() {
+      round.roundNumber++;
+      $('.game-container').css('display', 'none');
+      $('#modal-next-round').css('display', 'block');
+      eventHandlers.nextRound();
+    },
+  }
+
+// the actual function that starts a new round
+
+  const setUpRound = () => {
+    $('#show-round').text('Round: '+ round.roundNumber);
+    round.clearBoard();
+    round.generateShrooms(round.roundNumber*20);
+    round.setTimer();
+    eventHandlers.clickShrooms();
+  }
+
+// check whether user can move on to the next round
+
+  const checkForWin = () => {
+    if (score <= 0) {
+      round.roundNumber=1;
+      $('.game-container').css('display', 'none');
+      $('#modal-lose').css('display', 'block');
+      eventHandlers.startGame();
+    }  else if ( ((round.roundNumber === 1) && (score < 10)) ||((round.roundNumber===2) && (score<20)) || ((round.roundNumber=== 3 ) && (score < 30)) || ((round.roundNumber===4 ) && (score < 40)) || ((round.roundNumber=== 5 ) && (score < 50))  || ((round.roundNumber===6 ) && (score < 60)) || ((round.roundNumber===7 ) && (score < 70)) )  {
+      round.roundRepeat();
+    } else if ( ((score >= 10) && (round.roundNumber===1)) || ((score >= 20) && (round.roundNumber===2)) || ((score >= 30) && (round.roundNumber===3)) || ((score >= 40) && (round.roundNumber===4)) || ((score >= 50) && (round.roundNumber===5)) || ((score >= 60) && (round.roundNumber===6)) ) {
+      round.roundNext();
+    } else if ((round.roundNumber===7) && (score >= 70)) {
+      round.roundNumber=1;
+      $('.game-container').css('display', 'none');
+      $('#modal-win').css('display', 'block');
+      eventHandlers.startGame();
     }
-  },
-  setTimer() {
-    time=30;
-    const timer = setInterval( ()=> {
-      time--
-      console.log(time);
-      $('h3').text("Timer: " + time + " sec");
-      if (time===0) {
-        clearInterval(timer);
-          checkForWin();
-      }
-    }, 1000);
-  },
-}
-
-
-
-
-
-const setUpRound = () => {
-  $('#show-round').text('Round: '+ round.roundNumber);
-  round.clearBoard();
-  round.generateShrooms(round.roundNumber*20);
-  round.setTimer();
-  eventHandlers.clickShrooms();
-}
-
-
-
-
-
-
-const checkForWin = () => {
-  if (score <= 0) {
-    $('.game-container').css('display', 'none');
-    $('#modal-lose').css('display', 'block');
-    eventHandlers.go();
   }
-  if ((round.roundNumber === 1) && (score < 10)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  } else if ((score >= 10) && (round.roundNumber===1)) {
-    round.roundNumber=2;
-    $('.game-container').css('display', 'none');
-    $('#modal-next-round').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===2) && (score<20)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  }  else if ((score >= 20) && (round.roundNumber===2)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-next-round').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===3) && (score < 30)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  } else if ((score >= 30) && (round.roundNumber===3)) {
-    round.roundNumber=4;
-    $('.game-container').css('display', 'none');
-    $('#modal-next-round').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===4) && (score < 40)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  } else if ((score >= 40) && (round.roundNumber===4)) {
-    round.roundNumber=5;
-    $('.game-container').css('display', 'none');
-    $('#modal-next-round').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===5) && (score < 50)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  } else if ((score >= 50) && (round.roundNumber===5)) {
-    round.roundNumber=6;
-    $('.game-container').css('display', 'none');
-    $('#modal-next-round').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===6) && (score < 60)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  } else if ((score >= 60) && (round.roundNumber===6)) {
-    round.roundNumber=7;
-    $('.game-container').css('display', 'none');
-    $('#modal-next-round').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===7) && (score < 70)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-repeat').css('display', 'block');
-    eventHandlers.go();
-  } else if ((round.roundNumber===7) && (score >= 70)) {
-    $('.game-container').css('display', 'none');
-    $('#modal-win').css('display', 'block');
-  }
-}
 
+  // turn on event listener for start button when the page loads!
 
-
-eventHandlers.startGame();
-
+  eventHandlers.startGame();
 
 })
